f7b43dbf44555f09bcd066442cf1e29175806eed
Yahya751 <zenoscan8@gmail.com>
2026-09-21 21:21:33 +0500

    fix logout navigation

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { NavLink} from "react-router-dom";
+import { NavLink,useNavigate} from "react-router-dom";
 import { FaStethoscope, FaBars, FaXmark } from "react-icons/fa6";
 
 const navLinks = [
@@ -9,15 +9,14 @@ const navLinks = [
 ];
 
 export default function Navbar() {
+  const navigate=useNavigate()
   const [isOpen, setIsOpen] = useState(false);
   const isLoggedIn = localStorage.getItem("isLoggedIn") === "true";
-  const logout = () => {
-    localStorage.removeItem("isLoggedIn");
-    localStorage.removeItem("currentuser");
-
-    window.location.href = "/login";
-  };
-
+ const logout = () => {
+  localStorage.removeItem("isLoggedIn");
+  localStorage.removeItem("currentuser");
+  navigate("/login");
+};
   const linkClasses = ({ isActive }) =>
     `relative px-3 py-2 text-sm font-medium transition-all duration-200
     ${isActive ? "text-blue-600" : "text-gray-600 hover:text-blue-600"}`;
